8637337089d7972337f492117fdf1b79e3e20c5c
Austin Prete <austinprete23@gmail.com>
2020-06-06 18:29:09 -0600

    More instructions implemented

--- src/cpu.rs
+++ src/cpu.rs
@@ -31,7 +31,7 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
-        const CYCLES_TO_RUN: u8 = 20;
+        const CYCLES_TO_RUN: u16 = 400;
         let mut cycles_ran = 0;
         loop {
             if self.pc >= 4096 {
@@ -53,7 +53,7 @@ impl CPU {
         let opcode = ((self.memory[self.pc as usize] as u16) << 8) | self.memory[(self.pc + 1) as usize] as u16;
 
         let op1 = ((opcode & 0xF000) >> 12) as u8;
-        let op2 = ((opcode & 0x0F00) >> 8) as u8;
+        let op2 = ((opcode & 0x0F00) >> 8) as usize;
         let op3 = ((opcode & 0x00F0) >> 4) as u8;
         let op4 = (opcode & 0x000F) as u8;
 
@@ -69,23 +69,29 @@ impl CPU {
 
         match (op1, op2, op3, op4) {
             (0x0, 0x0, 0xE, 0x0) => panic! {"UNIMPLEMENTED OP"},
-            (0x0, 0x0, 0xE, 0xE) => panic! {"UNIMPLEMENTED OP"},
-            (0x0, _, _, _) => panic! {"UNIMPLEMENTED OP"},
+            (0x0, 0x0, 0xE, 0xE) => {
+                self.sp -= 1;
+                self.pc = self.stack[self.sp];
+            }
+            // This instruction only applies to original CHIP-8 hardware
+            (0x0, _, _, _) => {}
             (0x1, _, _, _) => panic! {"UNIMPLEMENTED OP"},
             (0x2, _, _, _) => {
                 self.stack[self.sp] = self.pc;
                 self.sp += 1;
 
                 self.pc = nnn;
-            },
+            }
             (0x3, x, _, _) => panic! {"UNIMPLEMENTED OP"},
             (0x4, x, _, _) => panic! {"UNIMPLEMENTED OP"},
             (0x5, x, y, 0) => panic! {"UNIMPLEMENTED OP"},
             (0x6, x, _, _) => {
-                self.v[x as usize] = nn;
+                self.v[x] = nn;
                 println!("{:?}", self.pc);
             }
-            (0x7, x, _, _) => panic! {"UNIMPLEMENTED OP"},
+            (0x7, x, _, _) => {
+                self.v[x] += nn;
+            }
             (0x8, x, y, 0x0) => panic! {"UNIMPLEMENTED OP"},
             (0x8, x, y, 0x1) => panic! {"UNIMPLEMENTED OP"},
             (0x8, x, y, 0x2) => panic! {"UNIMPLEMENTED OP"},
@@ -102,6 +108,33 @@ impl CPU {
             (0xB, _, _, _) => panic! {"UNIMPLEMENTED OP"},
             (0xD, x, y, n) => {
                 println!("Draw sprite {:?} at x={} y={}", n, x, y);
+            }
+            (0xF, x, 0x0, 0x7) => {
+                self.v[x] = self.delay_timer;
+            }
+            (0xF, x, 0x1, 0x5) => {
+                self.delay_timer = self.v[x];
+            }
+            (0xF, x, 0x2, 0x9) => {
+                self.i = self.v[x] as u16 * 5;
+            }
+            (0xF, x, 0x3, 0x3) => {
+                let mut num = self.v[x];
+
+                let hundreds = num / 100;
+                let tens = (num % 100) / 10;
+                let ones = (num % 10);
+
+                let start_address = self.i as usize;
+
+                self.memory[start_address] = hundreds;
+                self.memory[start_address] = tens;
+                self.memory[start_address] = ones;
+            }
+            (0xF, x, 0x6, 0x5) => {
+                for reg in 0..x {
+                    self.v[reg] = self.memory[(self.i as usize) + reg];
+                }
             }
             (_, _, _, _) => panic!("UNRECOGNIZED OP: {:?}", opcode)
         }
@@ -142,6 +175,11 @@ impl CPU {
             (0xA, _, _, _) => println!("Store memory address {:#05X?} to register I", nnn),
             (0xB, _, _, _) => println!("Jump to address {} + V0", nnn),
             (0xD, x, y, n) => println!("Draw sprite {:?} at x={} y={}", n, x, y),
+            (0xF, x, 0x0, 0x7) => println!("Set V{} = delay timer", x),
+            (0xF, x, 0x1, 0x5) => println!("Set delay timer = V{}", x),
+            (0xF, x, 0x2, 0x9) => println!("Set I = location of sprite for digit V{}.", x),
+            (0xF, x, 0x3, 0x3) => println!("Store BCD representation of V{} in memory locations {:#06X?}, {:#06X?}, and {:#06X?}.", x, self.i, self.i + 1, self.i + 2),
+            (0xF, x, 0x6, 0x5) => println!("Read registers V0 through V{} from memory starting at location {:#06X?}.", x, self.i),
             (_, _, _, _) => println!("UNRECOGNIZED OP: {:#06X?}", opcode)
         }
     }
